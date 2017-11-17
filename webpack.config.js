373252aa758cf5e60287fb062627bc367f5d956b
Daniel West <daniel@silverback.is>
2017-11-17 09:42:24 +0000

    Layout header and sticky footer

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,27 @@ var Encore = require('@symfony/webpack-encore');
 
 Encore
     .enablePostCssLoader()
-    // the project directory where compiled assets will be stored
-    .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
-    .setPublicPath('/build')
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
-
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .addEntry('js/app', './assets/js/app.js')
+    .addStyleEntry('css/app', './assets/css/app.sass')
+    .enableSassLoader()
 ;
+
+if (Encore.isProduction()) {
+  Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .enableVersioning()
+  ;
+} else {
+  Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('http://localhost:8080/')
+    .setManifestKeyPrefix('build/')
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps()
+  ;
+}
 
 module.exports = Encore.getWebpackConfig();
